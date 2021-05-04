edc0b812a621f05400d8d4bf2ea4c8284ce15fa9
mehmet kılınc <75855428+MehmetKilinc@users.noreply.github.com>
2021-05-04 20:58:23 +0300

    Add files via upload

---
+++ python modül oluşturma/fakt.py
@@ -0,0 +1,10 @@
+import modul1
+
+
+print(modul1.programlama_dilleri)
+
+
+print(modul1.toplama(1,2,3))
+
+
+print(modul1.selamla("ahmet"))
---
+++ python modül oluşturma/modul1.py
@@ -0,0 +1,23 @@
+programlama_dilleri = ["java" , "c" , "c++" , "python"]
+
+def toplama(a,b,c):
+
+    """
+
+    bu modül 3 tane sayıyı toplar
+
+    """
+    
+    return a + b + c
+
+def selamla(isim):
+
+
+    """
+
+    bu modül insanları selamlar
+
+
+    """
+    print("merhaba" , isim)
+
